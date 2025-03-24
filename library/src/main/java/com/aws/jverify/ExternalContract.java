fb5a0904531e71c422959a5b10824c46ca1d7f71
Remy Willems <rwillems@amazon.com>
2025-03-24 11:38:32 +0100

    Add documentation

--- library/src/main/java/com/aws/jverify/ExternalContract.java
+++ library/src/main/java/com/aws/jverify/ExternalContract.java
@@ -5,6 +5,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Let the definitions in the class this annotation is attached to,
+ * serve as a contract for the class this annotation references 
+ */
 @Retention(RetentionPolicy.SOURCE)
 @Target({ElementType.TYPE})
 public @interface ExternalContract {
