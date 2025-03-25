1db8ed6d123b1587c73fc89872ac58e326d586cf
olivier-aws <obouisso@amazon.com>
2025-03-25 11:26:10 -0400

    Ensure JVerify is robust to class with no constructors (#46)
    ### What was changed?
    - If a node does not originate from source, use the nearest surrounding
    origin instead. For example, a default constructor will get the origin
    of its containing class.
    - Fix bug in testing code to enable test files with public classes
    - Remove some code duplication in JavaToDafnyCompiler.java
    
    ### How has this been tested?
    - Added a test case in TestVerifier.java
    
    
    <small>By submitting this pull request, I confirm that my contribution
    is made under the terms of the
    [Apache-2.0](https://github.com/aws/jverify?tab=Apache-2.0-1-ov-file#readme).</small>
    
    ---------
    
    Co-authored-by: Remy Willems <rwillems@amazon.com>

--- verifier/src/test/java/com/aws/jverify/TestVerifier.java
+++ verifier/src/test/java/com/aws/jverify/TestVerifier.java
@@ -20,9 +20,9 @@ public class TestVerifier {
         var exitCode = run("UserProfile.java", true, writer);
         var output = canonicalizeNewlines(writer.toString());
         Assertions.assertEquals("""
-/test.java(32:9-32:16): Error: a postcondition could not be proved on this return path
-/test.java(23:21-23:26): Related location: this is the postcondition that could not be proved
-/test.java(25:16-25:77): Related location: this proposition could not be proved
+UserProfile.java(32:9-32:16): Error: a postcondition could not be proved on this return path
+UserProfile.java(23:21-23:26): Related location: this is the postcondition that could not be proved
+UserProfile.java(25:16-25:77): Related location: this proposition could not be proved
 
 Dafny program verifier finished with 7 verified, 1 error
 """, output);
@@ -33,7 +33,7 @@ Dafny program verifier finished with 7 verified, 1 error
         var directory = fromExamples 
                 ? Path.of("../examples/src/main/java/com/aws/verifier/examples") 
                 : Path.of("./src/test/java/com/aws/jverify");
-        var source = Files.readString(directory.resolve(inputFileName));
+        var filePath = directory.resolve(inputFileName);
         var dafnyPath = Path.of("../dafny").toAbsolutePath()
                 .resolve(IS_WINDOWS ? "Binaries/Dafny.exe" : "Scripts/dafny");
         var libraryJar = Path.of("../library/build/libs/library.jar");
@@ -41,10 +41,11 @@ Dafny program verifier finished with 7 verified, 1 error
         var options = new VerifierOptions(dafnyPath, libraryJar, prelude,
                 null, null, true,
                 new String[] { 
+                        "--use-basename-for-filename"
                         //"--wait-for-debugger"
                 } 
         );
-        return Driver.verifyJavaExample(options, source, writer);
+        return Driver.verifyJavaExample(options, filePath, writer);
     }
     
     @Test
@@ -52,14 +53,14 @@ Dafny program verifier finished with 7 verified, 1 error
         StringWriter writer = new StringWriter();
         var exitCode = run("AssertFalse.java", false, writer);
         var output = canonicalizeNewlines(writer.toString());
-        Assertions.assertEquals("/test.java(7:9-7:21): Error: assertion might not hold\n" +
+        Assertions.assertEquals("AssertFalse.java(7:9-7:21): Error: assertion might not hold\n" +
                 "\n" +
                 "Dafny program verifier finished with 2 verified, 1 error\n", output);
         Assertions.assertEquals(4, exitCode);
     }
 
     private static void checkErrorAt(String output, int line, int col) {
-        Assertions.assertTrue(output.contains("/test.java(" + line + ":" + col + "-"));
+        Assertions.assertTrue(output.contains("Operators.java(" + line + ":" + col + "-"));
     }
 
     @Test
@@ -97,11 +98,11 @@ Dafny program verifier finished with 7 verified, 1 error
         var exitCode = run("FibonacciInvalid.java", false, writer);
         var output = canonicalizeNewlines(writer.toString());
         Assertions.assertEquals("""
-/test.java(44:22-44:23): Error: value does not satisfy the subset constraints of 'nat32'
-/test.java(49:17-49:22): Error: value does not satisfy the subset constraints of 'nat32'
-/test.java(17:13-17:22): Error: a postcondition could not be proved on this return path
-/test.java(14:38-14:50): Related location: this is the postcondition that could not be proved
-/test.java(31:28-31:51): Error: value does not satisfy the subset constraints of 'int32'
+FibonacciInvalid.java(44:22-44:23): Error: value does not satisfy the subset constraints of 'nat32'
+FibonacciInvalid.java(49:17-49:22): Error: value does not satisfy the subset constraints of 'nat32'
+FibonacciInvalid.java(17:13-17:22): Error: a postcondition could not be proved on this return path
+FibonacciInvalid.java(14:38-14:50): Related location: this is the postcondition that could not be proved
+FibonacciInvalid.java(31:28-31:51): Error: value does not satisfy the subset constraints of 'int32'
 
 Dafny program verifier finished with 4 verified, 4 errors
 """, output);
@@ -141,6 +142,12 @@ Dafny program verifier finished with 5 verified, 0 errors
 
     }
 
+    @Test
+    public void testNoConstructor() throws IOException, InterruptedException {
+        StringWriter writer = new StringWriter();
+        var exitCode = run("NoConstructor.java", false, writer);
+        Assertions.assertEquals(0, exitCode);
+    }
     /**
      * Returns the text with all CRLF sequences replaced with LF.
      * This prevents erroneous failures of diff-based assertions on Windows platforms.
