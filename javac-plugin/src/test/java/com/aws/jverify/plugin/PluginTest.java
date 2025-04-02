ac4ca4536df17029859c781a7ea749fcadbf4d9b
Remy Willems <rwillems@amazon.com>
2025-04-02 21:05:15 +0200

    Cleanup

--- javac-plugin/src/test/java/com/aws/jverify/plugin/PluginTest.java
+++ javac-plugin/src/test/java/com/aws/jverify/plugin/PluginTest.java
@@ -98,6 +98,7 @@ public class PluginTest {
 
         CompilationSubject.assertThat(compilation).succeeded();
 
+        new Thread().run();
         var result = runMethodFromCompilation(compilation, "com.example.ModifyMe", "Bar");
         Assertions.assertEquals(3, result);
     }
