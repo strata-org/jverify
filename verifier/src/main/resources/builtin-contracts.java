ce41f958f501497a5c03c3400b13faa7c490d833
Remy Willems <rwillems@amazon.com>
2025-04-07 11:38:47 +0200

    Update built-in contracts

--- verifier/src/main/resources/builtin-contracts.java
+++ verifier/src/main/resources/builtin-contracts.java
@@ -1,6 +1,28 @@
 package com.aws.jverify;
 
+import java.lang.annotation.Native;
+
+@Contract(Short.class)
+class ShortContract {    /**
+ * A constant holding the minimum value a {@code short} can
+ * have, -2<sup>15</sup>.
+ */
+    public static final short   MIN_VALUE = -32768;
+    public static final short   MAX_VALUE = 32767;
+}
+
 @Contract(Integer.class)
 class IntegerContract {
     public static final int MAX_VALUE = 0x7fffffff;
+    public static final int MIN_VALUE = 0x80000000;
 }
+
+@Contract(Long.class)
+class LongContract {
+    public static final long MIN_VALUE = 0x8000000000000000L;
+    public static final long MAX_VALUE = 0x7fffffffffffffffL;
+}
+/**
+ * A constant holding the minimum value a {@code long} can
+ * have, -2<sup>63</sup>.
+ */
