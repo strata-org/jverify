2cd86d85fd360ba4052d3d43e5e4879c0bf8a16b
Olivier Bouissou <obouisso@amazon.com>
2025-05-01 11:51:50 -0400

    support of multidim arrays

---
+++ verifier/src/test/java/com/aws/jverify/verifier/tests/VerifyNewArray.java
@@ -0,0 +1,87 @@
+// TEST: exitCode=0 dafnyVerified=14 dafnyErrors=0
+
+package com.aws.jverify.verifier.tests;
+
+import com.aws.jverify.Pure;
+
+import static com.aws.jverify.JVerify.*;
+
+class Aux {
+    int a;
+    int b;
+    Aux(int a, int b) {
+        // Need a postcondition on the ctor to be able to prove better assertions in
+        // newAuxArray
+        this.a = a;
+        this.b = b;
+    }
+
+    public int getB() {
+        postcondition((Integer x) -> x == this.b);
+        return b;
+    }
+}
+
+
+@SuppressWarnings({"ConditionalBreakInInfiniteLoop", "StatementWithEmptyBody", "ConstantValue"})
+class VerifyNewArray {
+
+    void newIntArray() {
+        int[] arr = new int[5];
+        check(arr.length == 5);
+        arr[0] = 0;
+        for (var i = 1; i < 5; i++) {
+            modifies(arr);
+            invariant(arr[0] == 0);
+            int finalI = i;
+            // Crash with the invariant below
+            // invariant(forall((Integer j) -> implies(0 <= j && j < finalI, arr[j] == 0)));
+            arr[i] = i;
+        }
+        check(arr[0] == 0);
+    }
+
+
+
+
+
+    void newIntArray2() {
+        int[][] arr2 = new int[5][3];
+//        check(arr2.length == 5);
+//        check(arr2[0].length == 3);
+        arr2[0][0] = 0;
+        for (var i = 1; i < 5; i++) {
+            modifies(arr2);
+            invariant(arr2[0][0] == 0);
+            for (var j = 0; j < 3; j++) {
+                modifies(arr2);
+                invariant(arr2[0][0] == 0);
+
+                arr2[i][j] = i + j;
+            }
+        }
+        check(arr2[0][0] == 0);
+    }
+
+
+    void newAuxArray() {
+        var arr = new Aux[15];
+        check(arr.length == 15);
+        arr[0] = new Aux(0, 0);
+        int i;
+        for (i = 1; i < 15; i++) {
+            invariant(arr[0] != null);
+            modifies(arr);
+            int finalI = i;
+            // Crash with the invariant below
+            // invariant(forall((Integer j) -> implies(0<=j && j<finalI,arr[j] != null)));
+            arr[i] = new Aux(i, i + 1);
+        }
+        check(i == 15);
+    }
+
+    @Pure
+    boolean P(int x) {
+        return x > 10;
+    }
+}
--- verifier/src/test/java/com/aws/jverify/verifier/tests/VerifyStatements.java
+++ verifier/src/test/java/com/aws/jverify/verifier/tests/VerifyStatements.java
@@ -6,22 +6,6 @@ import com.aws.jverify.Pure;
 
 import static com.aws.jverify.JVerify.*;
 
-class Aux {
-    int a;
-    int b;
-    Aux(int a, int b) {
-        // Need a postcondition on the ctor to be able to prove better assertions in
-        // newAuxArray
-        this.a = a;
-        this.b = b;
-    }
-
-    public int getB() {
-        postcondition((Integer x) -> x == this.b);
-        return b;
-    }
-}
-
 
 @SuppressWarnings({"ConditionalBreakInInfiniteLoop", "StatementWithEmptyBody", "ConstantValue"})
 class VerifyStatements {
@@ -149,53 +133,6 @@ class VerifyStatements {
 
     void nativeAssert() {
         assert true;
-    }
-
-    void newIntArray() {
-        int[] arr = new int[5];
-        check(arr.length == 5);
-        arr[0] = 0;
-        for (var i = 1; i < 5; i++) {
-            modifies(arr);
-            invariant(arr[0] == 0);
-            int finalI = i;
-            // Crash with the invariant below
-            // invariant(forall((Integer j) -> implies(0 <= j && j < finalI, arr[j] == 0)));
-            arr[i] = i;
-        }
-        check(arr[0] == 0);
-    }
-
-    void newIntArray2() {
-        int[][] arr = new int[5][3];
-        check(arr.length == 5);
-        check(arr[0].length == 3);
-        arr[0][0] = 0;
-        for (var i = 1; i < 5; i++) {
-            modifies(arr);
-            invariant(arr[0][0] == 0);
-            for (var j = 0; j < 3; j++) {
-                modifies(arr);
-                arr[i][j] = i + j;
-            }
-        }
-        check(arr[0][0] == 0);
-    }
-
-    void newAuxArray() {
-        var arr = new Aux[15];
-        check(arr.length == 15);
-        arr[0] = new Aux(0, 0);
-        int i;
-        for (i = 1; i < 15; i++) {
-            invariant(arr[0] != null);
-            modifies(arr);
-            int finalI = i;
-            // Crash with the invariant below
-            // invariant(forall((Integer j) -> implies(0<=j && j<finalI,arr[j] != null)));
-            arr[i] = new Aux(i, i + 1);
-        }
-        check(i == 15);
     }
 
     @Pure
