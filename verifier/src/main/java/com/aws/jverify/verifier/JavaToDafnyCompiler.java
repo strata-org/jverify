dc7b4835b05b3710f29784396ccf6cc98a1aaee2
olivier-aws <obouisso@amazon.com>
2025-05-05 11:55:40 -0400

    Update verifier/src/main/java/com/aws/jverify/verifier/JavaToDafnyCompiler.java

--- verifier/src/main/java/com/aws/jverify/verifier/JavaToDafnyCompiler.java
+++ verifier/src/main/java/com/aws/jverify/verifier/JavaToDafnyCompiler.java
@@ -740,7 +740,7 @@ public class JavaToDafnyCompiler {
                 return new ExprDotName(origin, toExpr(fieldAccess.selected), getName(fieldAccess, fieldAccess.name), null);
             }
         } else if (expr instanceof JCTree.JCArrayAccess arrayAccess) {
-            // If subExpr is of type Array, this means we have an access to a full slice
+            // If expr is of type Array, this means we have an access to a full slice
             // of a multidimensional array, i.e. something like
             // int[][] tab = new int[4][4];
             // tab[1] = new int[4];
