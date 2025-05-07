8391ec835aee2c441de17f0555e339539eae6a3b
Olivier Bouissou <obouisso@amazon.com>
2025-05-07 17:56:24 -0400

    Improved testing

--- verifier/src/test/java/com/aws/jverify/verifier/tests/Constructors.java
+++ verifier/src/test/java/com/aws/jverify/verifier/tests/Constructors.java
@@ -9,10 +9,13 @@ import static com.aws.jverify.JVerify.*;
 class Constructors {
     public int f;
     public int g = 22;
+    public int h = 33;
     public Constructors() {
-        postcondition(this.g==22);
-        postcondition(this.f==3);
+        postcondition(this.g==22); // Value of g is set by the default initializer
+        postcondition(this.f==3);  // Value of f is set by the constructor
+        postcondition(this.h==4);  // Value of h is set by the default initializer then changed by the constructor
         f = 3;
+        h = 4;
     }
 
     public static void foo() {
