75a2b76943b8aea6532a43cac0baae4dd25db3b0
Remy Willems <rwillems@amazon.com>
2025-05-07 14:21:07 +0200

    Undo user profile change

--- examples/src/test/java/com/aws/jverify/examples/UserProfile.java
+++ examples/src/test/java/com/aws/jverify/examples/UserProfile.java
@@ -6,9 +6,59 @@ import com.aws.jverify.Nullable;
 import com.aws.jverify.Pure;
 
 class UserProfile {
-  void divZero() {
-    var x = 10 / 0;
-  }
+    public enum AccountType { Free, Premium }
+    public enum Theme { Light, Dark }
+    private AccountType accountType;
+    private @Nullable PremiumFeatures premiumFeatures;  // Should be non-null for Premium accounts
+
+    public UserProfile(AccountType accountType) {
+        this.accountType = accountType;
+        if (AccountType.Premium == accountType) {
+            this.premiumFeatures = new PremiumFeatures();
+        }
+    }
+
+    @Pure
+    @Invariant // Makes this a pre- and post-condition of all public methods
+    private boolean valid() {
+//                  ^^^^^ Related location: this is the postcondition that could not be proved
+        reads(this);
+        return accountType != AccountType.Premium || premiumFeatures != null;
+//             ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Related location: this proposition could not be proved
+    }
+
+    public void upgradeAccount() {
+        modifies(this);
+        this.accountType = AccountType.Premium;
+        // this.premiumFeatures = new PremiumFeatures();
+        return;
+//      ^^^^^^^ Error: a postcondition could not be proved on this return path
+    }
+
+    public boolean applyTheme(Theme theme) {
+        modifies(premiumFeatures);
+        if (AccountType.Premium == accountType) {
+            // Checker framework will report this as a possible null pointer exception. 
+            // JVerify accepts the code
+            premiumFeatures.setTheme(theme);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    static class PremiumFeatures {
+        private Theme theme;
+
+        public void setTheme(Theme theme) {
+            modifies(this);
+            this.theme = theme;
+        }
+
+        public Theme getTheme() {
+            return theme;
+        }
+    }
 }
 
 // TEST: exitCode=4 dafnyVerified=5 dafnyErrors=1
