00000ede1f70362a7f77eda555040f54f3400596
Olivier Bouissou <obouisso@amazon.com>
2025-05-09 11:14:31 -0400

    Allow new in return statement

---
+++ verifier/src/test/java/com/aws/jverify/verifier/tests/Allocate.java
@@ -0,0 +1,42 @@
+// TEST: exitCode=4 dafnyVerified=4 dafnyErrors=1
+
+package com.aws.jverify.verifier.tests;
+
+import com.aws.jverify.testengine.JVerifyTest;
+
+import com.aws.jverify.*;
+import static com.aws.jverify.JVerify.*;
+
+class Point {
+    private int a;
+    private int b;
+
+    public Point(int a_, int b_) {
+        postcondition(this.a == a_);
+        postcondition(this.b == b_);
+        this.a = a_;
+        this.b = b_;
+    }
+
+    @Pure
+    public int getA() {
+        reads(this);
+        return a;
+    }
+}
+
+// Class that test the support of array allocation and accesses
+@JVerifyTest
+class Allocate {
+
+    public static Point allocateInReturn(int a, int b) {
+        postcondition((Point p) -> fresh(p) &&  p.getA() == a);
+        return new Point(a, b);
+    }
+
+    public static void test(int a) {
+        Point p = allocateInReturn(a,2);
+        check(p.getA() == a);
+    }
+
+}
