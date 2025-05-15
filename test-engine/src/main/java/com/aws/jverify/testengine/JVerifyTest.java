447303a652309a7e4254c026b54a7bc4c7c63eb9
Robin Salkeld <salkeldr@amazon.com>
2025-05-15 12:24:11 -0700

    Docs on JVerifyTest

--- test-engine/src/main/java/com/aws/jverify/testengine/JVerifyTest.java
+++ test-engine/src/main/java/com/aws/jverify/testengine/JVerifyTest.java
@@ -7,12 +7,36 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Valid annotation values:
+ *
+ * <ol>
+ *      <li>
+ *          {@code @VerifyTest(exitCode = ...)}
+ *          - Verification should finish with exit code {@code X} without Dafny terminating normally
+ *          (i.e. Dafny is never invoked because there are javac errors, or Dafny terminates abnormally).
+ *      </li>
+ *      <li>
+ *          {@code @JVerifyTest(exitCode = ..., dafnyVerified = ..., dafnyErrors = ...)}
+ *          - Verification should finish with exit code {@code X}, Dafny terminates normally,
+ *          and Dafny's summary reports {@code Y} verified symbols and {@code Z} errors.
+ *      </li>
+ * </ol>
+ *
+ * Other parameters such as {@code verifyByDefault} can be set independently.
+ */
 @Testable
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ ElementType.TYPE })
 public @interface JVerifyTest {
+    /**
+     * What to pass for the --verify-by-default verifier option.
+     */
     boolean verifyByDefault() default true;
+
     int exitCode() default 0;
+
     int dafnyVerified() default -1;
+
     int dafnyErrors() default -1;
 }
