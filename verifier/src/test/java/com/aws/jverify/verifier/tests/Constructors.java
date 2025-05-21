48730a4d3fb890365cb59540a045021e0e3b0815
olivier-aws <obouisso@amazon.com>
2025-05-20 22:57:44 -0400

    Support method overloading (#144)
    <!-- Please remove these Markdown comments before publishing this PR,
    since the PR message is often used as the commit description.
    We only allow squash merging and GH suggests the PR details as a default
    commit message. -->
    
    ### What was changed?
    <!-- Is this a user-visible change? Remember to update RELEASE_NOTES.md
    -->
    
    Add support of method overloading in JVerify. This change adds a
    transformation pass that updates each method and field name of Java
    classes into a unique name, based on their type.
    
    ### How has this been tested?
    
    <small>By submitting this pull request, I confirm that my contribution
    is made under the terms of the
    [Apache-2.0](https://github.com/aws/jverify?tab=Apache-2.0-1-ov-file#readme).</small>
    
    ---------
    
    Co-authored-by: Robin Salkeld <salkeldr@amazon.com>

--- verifier/src/test/java/com/aws/jverify/verifier/tests/Constructors.java
+++ verifier/src/test/java/com/aws/jverify/verifier/tests/Constructors.java
@@ -1,13 +1,14 @@
 package com.aws.jverify.verifier.tests;
 
+import com.aws.jverify.Pure;
 import com.aws.jverify.testengine.JVerifyTest;
 import static com.aws.jverify.JVerify.*;
 
-@JVerifyTest(exitCode = 0, dafnyVerified = 2, dafnyErrors = 0)
+@JVerifyTest(exitCode = 4, dafnyVerified = 9, dafnyErrors = 1)
 class Constructors {
-    public int f;
-    public int g = 22;
-    public int h = 33;
+    private int f;
+    private int g = 22;
+    private int h = 33;
     public Constructors() {
         postcondition(this.g==22); // Value of g is set by the default initializer
         postcondition(this.f==3);  // Value of f is set by the constructor
@@ -16,9 +17,86 @@ class Constructors {
         h = 4;
     }
 
+    public Constructors(int f_) {
+        postcondition(this.f==f_ && this.g==22 && this.h==33);
+        f = f_;
+    }
+
+    public Constructors(int f_, int g_) {
+        postcondition(this.f==f_ && this.g==g_ && this.h==33);
+        f = f_;
+        g = g_;
+    }
+
+    public Constructors(int f_, int g_, int h_) {
+        postcondition(this.f==f_ && this.g==g_ && this.h==h_);
+        f = f_;
+        g = g_;
+        h = h_;
+    }
+
+    public Constructors(Constructors other) {
+        this.f = other.f;
+        this.g = other.g;
+        this.h = other.h;
+    }
+
+    @Pure
+    public int f() { reads(this); return f; }
+    @Pure
+    public int g() { reads(this); return g; }
+    @Pure
+    public int h() { reads(this); return h; }
+    @Pure
+    public int h(int x) {precondition(x<=100);return x+1;}
+
     public static void foo() {
-        Constructors c = new Constructors();
-        check(c.f==3);
-        check(c.g==22);
+        {
+            // Default constructor
+            Constructors c = new Constructors();
+            check(c.f()==3);
+            check(c.g()==22);
+            check(c.h()==4);
+        }
+        {
+            // Constructor with only one argument
+            Constructors c = new Constructors(10);
+            check(c.f()==10);
+            check(c.g()==22);
+            check(c.h()==33);
+        }
+        {
+            // Constructor with only two arguments
+            Constructors c = new Constructors(10, 15);
+            check(c.f()==10);
+            check(c.g()==15);
+            check(c.h()==33);
+        }
+        {
+            // Constructor with three arguments
+            Constructors c = new Constructors(10, 15, 20);
+            check(c.f()==10);
+            check(c.g()==15);
+            check(c.h()==20);
+        }
+        {
+            Constructors c = new Constructors(10, 15, 20);
+            int tmp = c.h();
+            int tmp2 = c.h(c.h());
+            check(tmp2 == tmp+1);
+        }
+        {
+            // Errors
+            Constructors c = new Constructors(10, 15, 20);
+            check(c.f()==11);
+//          ^^^^^^^^^^^^^^^^ Error: assertion might not hold
+            check(c.g()==16);
+            check(c.h()==21);
+        }
+        {
+            Constructors c = new Constructors(10, 15, 20);
+            Constructors c2 = new Constructors(c);
+            check(c2.f()==10);
+        }
     }
 }
