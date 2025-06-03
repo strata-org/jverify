cd220d677c6b2325c60746326def2e234e7a67ce
Remy Willems <rwillems@amazon.com>
2025-06-03 15:57:03 +0200

    Errors test now passes

--- verifier/src/test/java/com/aws/jverify/verifier/tests/MixedContractErrorsHeader.java
+++ verifier/src/test/java/com/aws/jverify/verifier/tests/MixedContractErrorsHeader.java
@@ -2,27 +2,28 @@ package com.aws.jverify.verifier.tests;
 
 import com.aws.jverify.Contract;
 import com.aws.jverify.ContractException;
-import com.aws.jverify.Pure;
 import com.aws.jverify.testengine.JVerifyTest;
 
-import static com.aws.jverify.JVerify.check;
 import static com.aws.jverify.JVerify.postcondition;
 
-@Contract(MixedContractErrors.class)
-@JVerifyTest(exitCode = 4, dafnyVerified = 10, dafnyErrors = 4)
-class MixedContractContractErrors implements MixedContractErrors {
+@JVerifyTest(exitCode = 2)
+public class MixedContractErrorsHeader {
+}
+
+@Contract(MixedContractErrorsI.class)
+class MixedContractErrorsC implements MixedContractErrorsI {
 
     @Override
     public int redeclaredContract() {
-//             ^ error
         postcondition((Integer r) -> true);
         throw new ContractException();
     }
 }
 
-interface MixedContractErrors {
-    
+interface MixedContractErrorsI {
+
     default int redeclaredContract() {
+//              ^ error: method redeclaredContract has both an extern- and internally defined contract, which is not allowed.
         postcondition((Integer r) -> r > 2);
         return 1;
     }
