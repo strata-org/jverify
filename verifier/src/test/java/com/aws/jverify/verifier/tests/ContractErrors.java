efb88c622344cccac1b19e4548b120304f730a92
Remy Willems <rwillems@amazon.com>
2025-06-03 16:07:52 +0200

    Update ContractErrors

--- verifier/src/test/java/com/aws/jverify/verifier/tests/ContractErrors.java
+++ verifier/src/test/java/com/aws/jverify/verifier/tests/ContractErrors.java
@@ -9,26 +9,25 @@ import static com.aws.jverify.JVerify.postcondition;
 @JVerifyTest(exitCode = 2)
 public class ContractErrors {}
 
-class Foo {
-//    ^ error: class 'Foo' may not be verified because it has an externally defined contract 
-    void bar(int x) {}
+abstract class Foo {
+    abstract void bar(int x);
 }
 
 @Contract(Foo.class)
-class FooContract1 {    
+class FooContract1 extends Foo {    
     void bar(int x) {
         check(x > 0);
     }
 
     void unusedExternalContract() {
-//       ^ error
+//       ^ error: Method 'unusedExternalContract' is part of a @Contract class, but it does not override any methods for which to provide a contract
         postcondition(false);
     }
 }
 
    @Contract(Foo.class)
 // ^ error: a class may not be referenced by more than one @Contract annotation
-class FooContract2 {
+class FooContract2 extends Foo {
     void bar(int x) {
         check(x < 0);
     }
