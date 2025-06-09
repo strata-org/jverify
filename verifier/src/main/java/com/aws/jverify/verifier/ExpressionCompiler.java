f9d42b9209309263ce0312b5380511ccc6f9e4ac
Remy Willems <rwillems@amazon.com>
2025-06-09 16:31:51 +0200

    Fix oopsd

--- verifier/src/main/java/com/aws/jverify/verifier/ExpressionCompiler.java
+++ verifier/src/main/java/com/aws/jverify/verifier/ExpressionCompiler.java
@@ -2,15 +2,18 @@ package com.aws.jverify.verifier;
 
 import com.aws.jverify.JVerify;
 import com.aws.jverify.generated.*;
+import com.sun.source.tree.Tree;
 import com.sun.tools.javac.code.Symbol;
 import com.sun.tools.javac.code.TypeTag;
 import com.sun.tools.javac.code.Types;
 import com.sun.tools.javac.tree.JCTree;
+import com.sun.tools.javac.tree.TreeInfo;
 import com.sun.tools.javac.tree.TreeMaker;
 import org.checkerframework.checker.nullness.qual.Nullable;
 
 import javax.lang.model.type.ArrayType;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 public class ExpressionCompiler {
@@ -145,8 +148,43 @@ public class ExpressionCompiler {
                     return jverifyMethodExpr;
                 }
 
-                var target = toExpr(invocation.getMethodSelect());
+                var methodSymbol = TreeInfo.symbol(invocation.getMethodSelect());
                 var argBindings = invocation.getArguments().stream().map(a -> new ActualBinding(null, toExpr(a), false)).toList();
+
+                if (methodSymbol.owner instanceof Symbol.ClassSymbol ownerClass
+                        && ownerClass.fullname.contentEquals(String.class.getName())) {
+                    final Expression receiver;
+                    if (invocation.getMethodSelect() instanceof JCTree.JCFieldAccess fieldAccess) {
+                        receiver = toExpr(fieldAccess.selected);
+                    } else {
+                        compiler.reportError(invocation, "notSupported", "method reference call");
+                        receiver = compiler.getHole(origin);
+                    }
+
+                    return switch (methodSymbol.name.toString()) {
+                        case "charAt" -> new SeqSelectExpr(origin, true, receiver,
+                                argBindings.getFirst().getActual(), null, null);
+                        case "equals" -> new BinaryExpr(origin, BinaryExprOpcode.Eq, receiver,
+                                argBindings.getFirst().getActual());
+                        case "isEmpty" -> new BinaryExpr(origin, BinaryExprOpcode.Eq, receiver,
+                                new SeqDisplayExpr(origin, List.of()));
+                        case "length" -> new UnaryOpExpr(
+                                origin, receiver, UnaryOpExprOpcode.Cardinality);
+                        case "substring" -> {
+                            var endIndex = argBindings.size() == 2
+                                    ? argBindings.get(1).getActual()
+                                    : new UnaryOpExpr(origin, receiver, UnaryOpExprOpcode.Cardinality);
+                            yield new SeqSelectExpr(origin, false, receiver,
+                                    argBindings.getFirst().getActual(), endIndex, null);
+                        }
+                        default -> {
+                            compiler.reportError(invocation, "notSupported", "String method " + methodSymbol);
+                            yield compiler.getHole(origin);
+                        }
+                    };
+                }
+
+                var target = toExpr(invocation.getMethodSelect());
                 return new ApplySuffix(origin, target, null,
                         new ActualBindings(argBindings), null);
             }
@@ -343,4 +381,43 @@ public class ExpressionCompiler {
         var hiExpr = hi == null ? null : toExpr(hi);
         return new SeqSelectExpr(origin, false, seqOrArrayExpr, loExpr, hiExpr, null);
     }
+
+    /**
+     * Translates the given string literal to a Dafny expression (of type {@code jstring}).
+     * For ease of debugging, the translation is the equivalent Dafny string literal
+     * if all characters in the string are printable ASCII or have (non-Unicode) escape sequences.
+     * Otherwise, the translation is a sequence display of the characters' numeric values.
+     */
+    private static Expression translateStringLiteral(IOrigin origin, JCTree.JCLiteral literal) {
+        assert literal.getKind().equals(Tree.Kind.STRING_LITERAL);
+        var stringValue = (String) literal.getValue();
+
+        var translatedChars = new StringBuilder();
+        for (var charValue : stringValue.toCharArray()) {
+            if (ESCAPED_CHARS.containsKey(charValue)) {
+                translatedChars.append(ESCAPED_CHARS.get(charValue));
+            } else if (charValue >= 0x20 && charValue <= 0x7e) {
+                translatedChars.append(charValue);
+            } else {
+                // Fallback to sequence of numeric values
+                var charExprs = stringValue.chars().boxed()
+                        .map(c -> (Expression) new LiteralExpr(origin, c)).toList();
+                return new SeqDisplayExpr(origin, charExprs);
+            }
+        }
+
+        var stringExpr = new StringLiteralExpr(origin, translatedChars.toString(), false);
+        return new ApplySuffix(origin, new NameSegment(origin, "JString", null), null,
+                new ActualBindings(List.of(new ActualBinding(null, stringExpr, false))), null);
+    }
+
+    private static final Map<Character, String> ESCAPED_CHARS = Map.of(
+            '\'', "\\'",
+            '\"', "\\\"",
+            '\\', "\\\\",
+            '\0', "\\0",
+            '\n', "\\n",
+            '\r', "\\r",
+            '\t', "\\t"
+    );
 }
