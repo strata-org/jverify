183d07dc9aee260c9cb0c65c0f632161c8bd60fb
Robin Salkeld <salkeldr@amazon.com>
2025-06-10 21:11:12 -0700

    invokedynamic translation documentation

--- verifier/src/main/java/com/aws/jverify/verifier/ExpressionCompiler.java
+++ verifier/src/main/java/com/aws/jverify/verifier/ExpressionCompiler.java
@@ -4,6 +4,7 @@ import com.aws.jverify.JVerify;
 import com.aws.jverify.generated.*;
 import com.sun.source.tree.Tree;
 import com.sun.tools.javac.code.Symbol;
+import com.sun.tools.javac.code.Symtab;
 import com.sun.tools.javac.code.TypeTag;
 import com.sun.tools.javac.code.Types;
 import com.sun.tools.javac.tree.JCTree;
@@ -435,13 +436,36 @@ public class ExpressionCompiler {
 
 
     private Expression translateDynamicMethod(IOrigin origin, JCTree source, Symbol.DynamicMethodSymbol dynamicMethodSymbol) {
-        // Translate to a method declaration
+        // invokedynamic in general is an invocation of a given "bootstrap method handle",
+        // with a subset of the arguments provided statically.
+        // javac translates lambda expressions and method references
+        // to invokedynamic calls to java.lang.invoke.LambdaMetafactory.metafactory,
+        // which is a method that creates factories of objects that implement single-method interfaces.
+        // The static arguments in this case identify the target interface and
+        // the synthetic static method that hols the lambda implementation.
+        // We can implement the same semantics
+        // via a Dafny datatype that extends the equivalent trait
+        // and a single data constructor that holds on to the static arguments
+        // and prepends them to the arguments to the static method.
+        // E.g.:
+        //
+        // datatype Lambda42 extends SomethingDoer = Lambda42(p0: int, p1: int) {
+        //   method doSomething(x: int, y: int) returns (r: int) {
+        //     r := doSomething$3(p0, p1, x, y);
+        //   }
+        // }
+
         var types = Types.instance(compiler.context);
         var names = Names.instance(compiler.context);
         var maker = TreeMaker.instance(compiler.context).at(source.pos);
+        var symtab = Symtab.instance(compiler.context);
+        if (dynamicMethodSymbol.bsm.owner.type != symtab.lambdaMetafactory
+              || dynamicMethodSymbol.bsm.name != names.metafactory) {
+            compiler.reportError(source, "notSupported", "invokedynamic on " + dynamicMethodSymbol.bsm);
+            return JavaToDafnyCompiler.getHole(compiler.toOrigin(source));
+        }
 
-        // TODO: Check this is a call to java.lang.invoke.LambdaMetafactory.metafactory (which we're assuming)
-
+        // Translate to a method declaration
         var interfaceType = dynamicMethodSymbol.dynamicType().getReturnType();
         var interfaceMethodSymbol = (Symbol.MethodSymbol) types.findDescriptorSymbol(interfaceType.tsym);
 
@@ -479,7 +503,6 @@ public class ExpressionCompiler {
         // Add a wrapper datatype with that method declaration to the outer scope
         var datatypeName = "Lambda" + compiler.lambdaDatatypeDecls.size();
         var datatypeNameNode = new Name(origin, datatypeName);
-        // TODO: Need constructor parameters this time
         List<Formal> datatypeCtorParams = params.stream().map(p ->
                 new Formal(origin, compiler.getName(p, p.name), compiler.translateType(p.type, origin), false, true,
                         null, null, false, false, false, null)).toList();
