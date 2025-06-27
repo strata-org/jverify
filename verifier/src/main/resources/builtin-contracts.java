add5fc366bf4e303ed663723d9eb46d42fe29e18
Olivier Bouissou <obouisso@amazon.com>
2025-06-27 13:21:08 -0400

    f

--- verifier/src/main/resources/builtin-contracts.java
+++ verifier/src/main/resources/builtin-contracts.java
@@ -1,8 +1,15 @@
 package com.aws.jverify.builtin;
 
+import com.aws.jverify.Erased;
 import com.aws.jverify.Contract;
+import com.aws.jverify.Pure;
+import com.aws.jverify.Nat;
+import com.aws.jverify.Unbounded;
+import static com.aws.jverify.JVerify.*;
+
 import com.aws.jverify.ContractException;
 
+import java.math.BigInteger;
 import java.util.Collection;
 import java.util.List;
 import java.util.SequencedCollection;
@@ -60,3 +67,72 @@ class LongContract {
     public static final long MIN_VALUE = 0x8000000000000000L;
     public static final long MAX_VALUE = 0x7fffffffffffffffL;
 }
+/*
+class HelperForBigIntegerContract {
+
+    @Erased
+    @Pure
+    static boolean isAllDigits(String v) {
+        return forall((Integer i) -> !(0<=i && i<v.length()) || (v.charAt(i) >= '0' && v.charAt(i) <= '9'));
+    }
+
+    @Erased
+    @Pure
+    static boolean isValidString(String v) {
+        return (v.length() == 0 ||
+                ((v.charAt(0) == '+' || v.charAt(0) == '-') && isAllDigits(v.substring(1))) ||
+                isAllDigits(v));
+    }
+    @Erased
+    @Pure
+    static @Unbounded int stringToInt(String v) {
+        decreases(v.length());
+        return
+                v.length() == 0 ? 0 :
+                        (v.charAt(0) == '+' ?
+                        stringToInt(v.substring(1)) :
+                        (v.charAt(0) == '-' ? -stringToInt(v.substring(1)) :
+                                (v.charAt(v.length()-1)-'0') + 10*stringToInt(v.substring(0,v.length()-1))
+                                ));
+    }
+
+    @Pure
+    static @Unbounded int value(BigIntegerContract bc) {
+        return bc.value;
+    }
+}
+
+@Contract(BigInteger.class)
+class BigIntegerContract {
+
+    public @Unbounded int value;
+
+    // Required for other constructors to be taken into account.
+    public BigIntegerContract() {
+    }
+
+    public BigIntegerContract(String val) {
+        precondition(HelperForBigIntegerContract.isValidString(val));
+        postcondition(value == HelperForBigIntegerContract.stringToInt(val));
+        throw new ContractException();
+    }
+
+    @Pure
+    public int intValue() {
+        reads(this);
+        precondition(value >= Integer.MIN_VALUE && value <= Integer.MAX_VALUE);
+        postcondition((Integer r) -> r == value);
+        throw new ContractException();
+    }
+
+    @Pure
+    public BigInteger add(BigInteger v) {
+        reads(this);
+        reads(v);
+        postcondition((BigInteger b) -> HelperForBigIntegerContract.value(b) == HelperForBigIntegerContract.value(this) + HelperForBigIntegerContract.value(v));
+        throw new ContractException();
+    }
+
+}
+
+ */
