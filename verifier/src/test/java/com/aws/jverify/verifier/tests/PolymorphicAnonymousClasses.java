b531dfccfb23146fa6215a8040ace0f0c3588752
Remy Willems <rwillems@amazon.com>
2025-07-28 16:50:53 +0200

    Added tests

---
+++ verifier/src/test/java/com/aws/jverify/verifier/tests/PolymorphicAnonymousClasses.java
@@ -0,0 +1,57 @@
+package com.aws.jverify.verifier.tests;
+
+import com.aws.jverify.testengine.JVerifyTest;
+
+@JVerifyTest(exitCode = 0, dafnyVerified = 4, dafnyErrors = 0,
+        verifyPrintedDafny = true)
+public class PolymorphicAnonymousClasses {
+
+    class Anything {}
+    void lambdaForGenericInterfaces() {
+        var fooArg = new MyConsumer<Anything>() {
+            @Override
+            public int consume(Anything value) {
+                return 3;
+            }
+        };
+        foo(fooArg);
+        
+        var barArg = new MyProducer<Anything>() {
+            @Override
+            public Anything produce() {
+                return new Anything();
+            }
+        };
+        bar(barArg);
+    }
+
+//    <U> void lambdaForGenericMethod() {
+//        var zazArg = new MyConsumer<U>() {
+//            @Override
+//            public int consume(U value) {
+//                return 3;
+//            }
+//        };
+//        zaz(zazArg);
+//    }
+
+    void foo(MyConsumer<Anything> f) {}
+    void bar(MyProducer<Anything> f) {}
+    //<R> void zaz(MyConsumer<R> f) {}
+
+//    static class GenericClass<U> {
+//        void lambdaForGenericClass() {
+//            tar((U e) -> 3);
+//        }
+//
+//        <R> void tar(MyConsumer<R> f) {}
+//    }
+
+    interface MyConsumer<T> {
+        int consume(T value);
+    }
+
+    interface MyProducer<T> {
+        T produce();
+    }
+}
