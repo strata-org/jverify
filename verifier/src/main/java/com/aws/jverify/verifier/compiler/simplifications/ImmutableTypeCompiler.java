243d73a1931a0912eb16744bd5b0411a5b2bf94e
Remy Willems <rgv.willems@gmail.com>
2025-08-22 09:56:25 +0200

    Handle `@Contract` as a separate pass (#298)
    ### What was changed?
    - Handle `@Contract` as a separate pass, so all other passes don't need
    to be aware of `@Contract` semantics.
    - I started this PR because I was working on
    https://github.com/aws/jverify/pull/296 and realized there too I had to
    consider contracts. I think with this refactoring, lots of future PRs
    will be easier.
    - The separate pass tries to make it seem as if all contracts defined by
    a `@Contract` class, were defined on the contractee class. It inserts
    `@Verify(false)` annotations so that the bodies of those methods does
    not need to be checked, so the `throw new ContractException()` is
    ignored.
    - Move some reporting code from `JavaToDafnyCompiler` to a new class
    `Reporter`
    
    ### How has this been tested?
    - Uses existing tests
    
    <small>By submitting this pull request, I confirm that my contribution
    is made under the terms of the
    [Apache-2.0](https://github.com/aws/jverify?tab=Apache-2.0-1-ov-file#readme).</small>

--- verifier/src/main/java/com/aws/jverify/verifier/compiler/simplifications/ImmutableTypeCompiler.java
+++ verifier/src/main/java/com/aws/jverify/verifier/compiler/simplifications/ImmutableTypeCompiler.java
@@ -37,7 +37,7 @@ public class ImmutableTypeCompiler {
         Symbol.ClassSymbol currentTypeSymbol = typeDeclarationCompiler.getCurrentTypeSymbol(classDecl.sym);
         var traits = currentTypeSymbol
                 .getInterfaces().stream()
-                .filter(compiler::typeHasAContract)
+                .filter(type -> JavaToDafnyCompiler.typeHasSource(compiler.index, type.tsym))
                 .map(baseType -> compiler.translateType(baseType, origin, null))
                 .collect(Collectors.toList());
         
@@ -47,7 +47,7 @@ public class ImmutableTypeCompiler {
             if (superClass.tsym == symtab.objectType.tsym || superClass.getKind() == TypeKind.NONE) {
                 traits.addFirst(new UserDefinedType(origin, new NameSegment(origin, JavaToDafnyCompiler.REFERENCE_OR_VALUE_OBJECT_NAME, null)));
             } else {
-                if (compiler.typeHasAContract(superClass)) {
+                if (JavaToDafnyCompiler.typeHasSource(compiler.index, superClass.tsym)) {
                     traits.addFirst(compiler.translateType(superClass, origin, null));
                 }
             }
--- verifier/src/test/java/com/aws/jverify/verifier/tests/jcl/PrimitiveAndBoxedTypesVerification.java
+++ verifier/src/test/java/com/aws/jverify/verifier/tests/jcl/PrimitiveAndBoxedTypesVerification.java
@@ -1,11 +1,12 @@
 package com.aws.jverify.verifier.tests.jcl;
 
+import com.aws.jverify.Nullable;
 import com.aws.jverify.Pure;
 import com.aws.jverify.testengine.JVerifyTest;
 
 import static com.aws.jverify.JVerify.check;
 
-@JVerifyTest(exitCode = 4, dafnyVerified = 3, dafnyErrors = 1, useBuiltinContracts = true)
+@JVerifyTest(exitCode = 4, dafnyVerified = 4, dafnyErrors = 1, useBuiltinContracts = true)
 class PrimitiveAndBoxedTypesVerification {
     @Pure
     static boolean boxedIsZero(Integer i) {
@@ -13,9 +14,14 @@ class PrimitiveAndBoxedTypesVerification {
     }
 
     @SuppressWarnings("ConstantValue")
+    static void useZero() {
+        Integer zero = 0;
+        check(boxedIsZero(zero));
+    }
+
+    @SuppressWarnings("ConstantValue")
     static void nullArgForNonNullParam() {
         Integer zero = null;
 //                     ^^^^ Error: value of expression (of type 'Integer?') is not known to be an instance of type 'Integer', because it might be null
-        check(boxedIsZero(zero));
     }
 }
