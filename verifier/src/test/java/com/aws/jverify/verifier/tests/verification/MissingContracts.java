c306aa3001a6e0d9bc4189d0f5c7b6389bbf8856
Remy Willems <rwillems@amazon.com>
2025-08-25 13:47:22 +0200

    Give a body to unverified methods

--- verifier/src/test/java/com/aws/jverify/verifier/tests/verification/MissingContracts.java
+++ verifier/src/test/java/com/aws/jverify/verifier/tests/verification/MissingContracts.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 import static com.aws.jverify.JVerify.check;
 
-@JVerifyTest(exitCode = 4, dafnyVerified = 6, dafnyErrors = 1)
+@JVerifyTest(exitCode = 4, dafnyVerified = 8, dafnyErrors = 1)
 public class MissingContracts {
     
     void checkFalse() {
