299a378db60cafae70bb1b24210708cb6671509c
Oyendrila Dobe <dobeoyen@amazon.com>
2025-08-27 11:04:38 -0400

    Added contract for Optional Class (#297)
    ### What was changed?
    - Added contract for a subset of methods in Optional class 
    - Modified relevant line number for expected error in `Collections.test`
    
    
    ### How has this been tested?
    - Added relevant test cases in `verifier/tests/Optionals.java`
    
    <small>By submitting this pull request, I confirm that my contribution
    is made under the terms of the
    [Apache-2.0](https://github.com/aws/jverify?tab=Apache-2.0-1-ov-file#readme).</small>

--- verifier/src/main/resources/builtin-contracts.java
+++ verifier/src/main/resources/builtin-contracts.java
@@ -26,6 +26,8 @@ import java.util.Collection;
 import java.util.List;
 import java.util.SequencedCollection;
 
+import java.util.Optional;
+
 @Contract(Comparable.class)
 class ComparableContract<T> {
 }
@@ -520,4 +522,59 @@ class BigIntegerContract  {
         throw new ContractException();
     }
 
+}
+
+@Contract(value = Optional.class, immutable = true)
+class OptionalContract<T> {
+
+    private T value;
+    private boolean isSet;
+
+    //TODO: add precondition for null
+    @Pure
+    static <T> OptionalContract<T> of(T value) {
+        postcondition((OptionalContract<T> o) -> o.value == value && o.isSet);
+        throw new ContractException();
+    }
+
+    @Pure
+    static <T> OptionalContract<T> empty() {
+        postcondition((OptionalContract<T> o) -> !o.isSet);
+        throw new ContractException();
+    }
+
+    //Currently the same as of because we are not able to handle null cases
+    //TODO: add null check
+    /*
+    @Pure
+    static <T> OptionalContract<T> ofNullable(T value) {
+        postcondition((OptionalContract<T> o) ->  o.isSet && o.value == value);
+        throw new ContractException();
+    }
+    */
+
+    @Pure
+    boolean isEmpty() {
+        postcondition((boolean r) -> r == !isSet);
+        throw new ContractException();
+    }
+
+    @Pure
+    boolean isPresent() {
+        postcondition((boolean r) -> r == isSet);
+        throw new ContractException();
+    }
+
+    @Pure
+    T orElse(T value) {
+        postcondition((T b) -> isPresent()? b == this.value : b == value);
+        throw new ContractException();
+    }
+
+    @Pure
+    T get() {
+        precondition(isSet);
+        postcondition((T b) -> b == this.value);
+        throw new ContractException();
+    }
 }
---
+++ verifier/src/test/java/com/aws/jverify/verifier/tests/Optionals.java
@@ -0,0 +1,49 @@
+// ^ /builtin-contracts.java(576:22-576:27) Related location: this proposition could not be proved
+package com.aws.jverify.verifier.tests;
+
+import com.aws.jverify.testengine.JVerifyTest;
+
+import java.util.Optional;
+
+import static com.aws.jverify.JVerify.check;
+
+@JVerifyTest(exitCode = 4, dafnyVerified = 3, dafnyErrors = 2, useBuiltinContracts = true)
+public class Optionals {
+
+    void testEmpty() {
+        var opt = Optional.empty();
+        check(opt.isEmpty());
+        check(!opt.isPresent());
+        var res = opt.get();
+//                ^^^^^^^^^ Error: function precondition could not be proved
+        var res1 = opt.orElse("Hello");
+        check(res1.equals("Hello"));
+    }
+
+    void testOf() {
+        var opt = Optional.of("Hello");
+        check(opt.get().equals("Hello"));
+        check(opt.isEmpty());
+//      ^^^^^^^^^^^^^^^^^^^^ Error: assertion might not hold
+        check(opt.isPresent());
+        var res = opt.orElse("World");
+        check(res.equals("Hello"));
+    }
+
+    // will re-enable once we can handle null checks on generic classes
+    /*
+    void testOfNullable() {
+        var opt = Optional.ofNullable("5");
+        check(opt.isPresent());
+        check(opt.get().equals("7"));
+            ^^^^^^^^^^^^^^^^^^^^^ Error: assertion might not hold
+        check(opt.get().equals("5"));
+        check(opt.get().equals(opt.get()));
+    }
+ */
+
+}
+
+
+
+
