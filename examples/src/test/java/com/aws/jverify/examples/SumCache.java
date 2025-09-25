3427132a137b1f71765ef9060d33c9c5637970fa
Remy Willems <rwillems@amazon.com>
2025-09-25 16:55:53 +0200

    Updates

--- examples/src/test/java/com/aws/jverify/examples/SumCache.java
+++ examples/src/test/java/com/aws/jverify/examples/SumCache.java
@@ -3,19 +3,21 @@ package com.aws.jverify.examples;
 import com.aws.jverify.Invariant;
 import com.aws.jverify.Pure;
 import java.util.ArrayList;
+import java.util.function.BinaryOperator;
 
 import static com.aws.jverify.JVerify.*;
 
 public class SumCache {
     ArrayList<Integer> numbers = new ArrayList<>();
     int sum = 0;
+    final static BinaryOperator<Integer> integerSum = Integer::sum;
 
     @Pure
     @Invariant
     boolean validSum() {
         reads(this);
         reads(numbers);
-        return numbers.stream().reduce(0, Integer::sum) == sum;
+        return numbers.stream().reduce(0, integerSum) == sum;
 //          ^^^^^^^^ Related: this is the invariant that could not be proven
     }
     
@@ -24,9 +26,16 @@ public class SumCache {
         modifies(this);
         modifies(numbers);
 
-        int before = numbers.stream().reduce(0, Integer::sum);
-        int before2 = numbers.stream().reduce(0, Integer::sum);
-        check(before == before2);
+        BinaryOperator<Integer> a1 = integerSum;
+        BinaryOperator<Integer> a2 = integerSum;
+        check(a1 == a2);
+        int before = numbers.stream().reduce(0, a1);
+        int before2 = numbers.stream().reduce(0, a2);
+        check(before == before2); 
+        // fails because the two lambdas are different
+        // we could try to let the two method references re-use the same lambda,
+        // but we want this to work even if we had used two separate but equivalent lambdas
+        // do we need 
 
         check(before == sum);
         sum = sum + value;
