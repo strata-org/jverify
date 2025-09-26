2b5fcec2cbec148b2ae0fcc2fc38cc9704c6c8cd
Remy Willems <rwillems@amazon.com>
2025-09-26 15:42:24 +0200

    Fixes

--- verifier/src/test/java/com/aws/jverify/verifier/tests/Optionals.java
+++ verifier/src/test/java/com/aws/jverify/verifier/tests/Optionals.java
@@ -1,4 +1,4 @@
-// ^ /builtin-contracts.java(650:22-650:27) Related location: this proposition could not be proved
+// ^ /builtin-contracts.java(697:22-697:27) Related location: this proposition could not be proved
 package com.aws.jverify.verifier.tests;
 
 import com.aws.jverify.testengine.JVerifyTest;
