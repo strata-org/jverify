111b073b932ee68909cb46f162782293f944f580
Robin Salkeld <salkeldr@amazon.com>
2025-10-29 15:10:45 -0700

    language version

--- sample-project/app/build.gradle.kts
+++ sample-project/app/build.gradle.kts
@@ -60,7 +60,7 @@ fun createJavacExports(targets: List<String>): List<String> {
 // Apply a specific Java toolchain to ease working on different environments.
 java {
     toolchain {
-        languageVersion = JavaLanguageVersion.of(17)
+        languageVersion = JavaLanguageVersion.of(24)
     }
 }
 
