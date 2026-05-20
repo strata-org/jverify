6b69911f30ecfd8e94e9e07bec21d6c7bee9bbbe
Olivier Bouissou <obouisso@amazon.com>
2026-05-20 18:43:24 +0000

    test: remove break/continue tests from VerifyStatements
    Remove whileWithBreak, whileWithContinue, forLoopBreak, forLoopContinue,
    nestedForLoopBreak, and twoForLoops from VerifyStatements. These are now
    covered by VerifyBreakContinue which uses static methods that actually
    exercise the Laurel compiler code paths. The removed tests were instance
    methods that never went through the compiler (due to the Flags.STATIC
    gate), making them effectively no-ops.
    
    Kept in VerifyStatements:
    - nestedForLoopContinue: uses labeled 'continue outerLoop' (unique)
    - doWhileLoop: uses 'decreases' clause (unique)
    - forLoop, nestedForLoop: test loops without break/continue
    - skip, nativeAssert, P/P2/P3, underscoreVariableName,
      methodWithResult, ignoreCallResult: unrelated to break/continue

--- verifier/src/test/java/org/strata/jverify/verifier/tests/javasupport/statements/VerifyStatements.java
+++ verifier/src/test/java/org/strata/jverify/verifier/tests/javasupport/statements/VerifyStatements.java
@@ -6,61 +6,11 @@ import org.strata.jverify.testengine.JVerifyTest;
 import static org.strata.jverify.JVerify.*;
 
 @SuppressWarnings({"ConditionalBreakInInfiniteLoop", "StatementWithEmptyBody", "ConstantValue"})
-@JVerifyTest(methodsVerified = 19, errorCount = 0)
+@JVerifyTest(methodsVerified = 13, errorCount = 0)
 class VerifyStatements {
-    void whileWithBreak() {
-        var x = 0;
-        while(true) {
-            decreases(10 - x);
-            if (P(x)) {
-                break;
-            }
-            x = x + 1;
-        }
-    }
-    
-    void whileWithContinue() {
-        var x = 0;
-        while(x < 10) {
-            if (P(x)) {
-                continue;
-            }
-            check(!P(x));
-            x = x + 1;
-        }
-    }
-    
     void forLoop() {
         int i = 0;
         for(i = 0; i < 5; i = i + 1) {
-        }
-        check(i == 5);
-    }
-
-    void forLoopBreak() {
-        int i = 0;
-        int x = 0;
-        for(i = 0; true; i = i + 1) {
-            decreases(5 - i);
-            invariant(i <= 5);
-            invariant(x == i);
-            if (i == 5) {
-                break;
-            }
-            x = x + 1;
-        }
-        check(i == 5);
-    }
-
-    void forLoopContinue() {
-        int i = 0;
-        int x = 0;
-        for(i = 0; i < 5; i = i + 1) {
-            invariant(i <= 3 ? x == i : x == i - 1);
-            if (i == 3) {
-                continue;
-            }
-            x = x + 1;
         }
         check(i == 5);
     }
@@ -95,26 +45,6 @@ class VerifyStatements {
         }
     }
 
-    void nestedForLoopBreak() {
-        int x = 0;
-        outerLoop:
-        for (int i = 0; i < 5; i = i + 1) {
-            invariant(x == i * 5);
-            invariant(i <= 2);
-
-            for (int j = 0; j < 5; j = j + 1) {
-                invariant(x == j + i * 5);
-                invariant(i == 2 ? j <= 2 : true);
-                if (i == 2 && j == 2) {
-                    check(x == 12);
-                    break outerLoop;
-                }
-                x = x + 1;
-            }
-        }
-        check(x == 12);
-    }
-    
     void doWhileLoop() {
         int x = 0;
         do {
@@ -125,20 +55,6 @@ class VerifyStatements {
         check(x == 5);
     }
 
-    void twoForLoops() {
-        int cnt = 0;
-        for (var i = 0; i < 10; i++) {
-            invariant(cnt==i);
-            cnt++;
-        }
-        check(cnt==10);
-        for (var i = 0; i < 10; i++) {
-            invariant(cnt-10==i);
-            cnt++;
-        }
-        check(cnt==20);
-    }
-    
     void skip() {
         ;;;;
         check(true);
