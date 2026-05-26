8adc6f3ed51c4d6b7921a2a2b5c5563a2b407c7e
Olivier Bouissou <obouisso@amazon.com>
2026-05-26 21:32:11 +0000

    improved test for return inside a loop

--- verifier/src/test/java/org/strata/jverify/verifier/tests/javasupport/statements/VerifyBreakContinue.java
+++ verifier/src/test/java/org/strata/jverify/verifier/tests/javasupport/statements/VerifyBreakContinue.java
@@ -190,14 +190,15 @@ class VerifyBreakContinue {
         check(x == 3);
     }
 
-    static int returnInsideLoop() {
+    static boolean returnInsideLoop() {
+	postcondition((Boolean b) -> b == true);
         for (int i = 0; i < 10; i = i + 1) {
             invariant(i >= 0 && i <= 5);
             if (i == 5) {
-                return i;
+                return true;
             }
         }
-        return 0;
+        return false;
     }
 
     static void breakBad() {
